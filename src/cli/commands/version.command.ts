471eae7195af2674cfbd146d118116cada518279
Petrov <dp200308@gmail.com>
2024-03-05 11:34:14 +0500

    fix: reworked version command

--- src/cli/commands/version.command.ts
+++ src/cli/commands/version.command.ts
@@ -1,34 +1,9 @@
-import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
 import { Command } from './command.interface.js';
-
-type PackageJSONConfig = {
-  version: string;
-}
-
-function isPackageJSONConfig(value: unknown): value is PackageJSONConfig {
-  return (
-    typeof value === 'object' &&
-    value !== null &&
-    !Array.isArray(value) &&
-    Object.hasOwn(value, 'version')
-  );
-}
+import packageJSON from '../../../package.json' assert {type: 'json'};
 
 export class VersionCommand implements Command {
-  constructor(
-    private readonly filePath: string = './package.json'
-  ) {}
-
   private readVersion(): string {
-    const jsonContent = readFileSync(resolve(this.filePath), 'utf-8');
-    const importedContent: unknown = JSON.parse(jsonContent);
-
-    if (! isPackageJSONConfig(importedContent)) {
-      throw new Error('Failed to parse json content.');
-    }
-
-    return importedContent.version;
+    return packageJSON.version;
   }
 
   public getName(): string {
@@ -36,15 +11,7 @@ export class VersionCommand implements Command {
   }
 
   public async execute(..._parameters: string[]): Promise<void> {
-    try {
-      const version = this.readVersion();
-      console.info(version);
-    } catch (error: unknown) {
-      console.error(`Не удалось узнать название из файла ${this.filePath}`);
-
-      if (error instanceof Error) {
-        console.error(error.message);
-      }
-    }
+    const version = this.readVersion();
+    console.info(version);
   }
 }
