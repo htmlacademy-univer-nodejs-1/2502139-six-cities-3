9548d523424aa8eba59bf65c35085dd51be167a0
Petrov <dp200308@gmail.com>
2024-04-15 15:30:58 +0500

    refactor: add more methods to user module

--- src/shared/modules/user/user-service.interface.ts
+++ src/shared/modules/user/user-service.interface.ts
@@ -1,10 +1,15 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { UserEntity } from './user.entity.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
+import { UpdateUserDto } from './dto/update-user.dto.js';
 
 export interface UserService {
   create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
   findById(id: string): Promise<DocumentType<UserEntity> | null>;
   findByEmail(email: string): Promise<DocumentType<UserEntity> | null>;
   findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
+  updateById(id: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null>
+  updateByEmail(email: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null>
+  deleteById(id: string): Promise<DocumentType<UserEntity> | null>
+  deleteByEmail(email: string): Promise<DocumentType<UserEntity> | null>
 }
