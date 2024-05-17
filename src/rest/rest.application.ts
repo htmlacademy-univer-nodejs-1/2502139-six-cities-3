d48912864ff7b20b9d6e3f83e9a56a159eab1f73
Danila Petrov <dp200308@gmail.com>
2024-05-17 14:38:15 +0500

    refactor: add express implementation to RestApplication class

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,15 +5,20 @@ import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
+import express, { Express } from 'express';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient)
     private readonly databaseClient: DatabaseClient,
-  ) {}
+  ) {
+    this.server = express();
+  }
 
   private async _initDb() {
     const mongoUri = getMongoURI(
@@ -27,14 +32,20 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('Приложение инициализировано');
-    this.logger.info(
-      `Значение из переменной окружений PORT: ${this.config.get('PORT')}`
-    );
 
     this.logger.info('Инициализация базы данных...');
     await this._initDb();
     this.logger.info('База данных инициализирована');
+
+    this.logger.info('Инициализация сервера Express...');
+    await this._initServer();
+    this.logger.info(`Сервер Express успешно запущен по адресу http://localhost:${this.config.get('PORT')}`);
   }
 }
